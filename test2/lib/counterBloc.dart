0f410e7152fcb8547a665a63c2a8799c5dfee8da
aca <akhilorappady@gmail.com>
2020-08-05 17:53:45 +0530

    added some comments

--- test2/lib/counterBloc.dart
+++ test2/lib/counterBloc.dart
@@ -1,9 +1,10 @@
 import 'package:bloc/bloc.dart';
 
 enum CounterEvents { increment, decrement }
-
+// the second argument in Bloc<> is actually state. in  this case it is integer.
+// but it can be class as well
 class CounterBloc extends Bloc<CounterEvents, int> {
-  CounterBloc() : super(0);
+  CounterBloc() : super(0); //initial state. This replces get initialState() in tutorial videos
 
   @override
   Stream<int> mapEventToState(CounterEvents event) async* {
